9577f9876594564ad85ed0d1f6c04ec95817b154
kmseunh <tmdgus8779@gmail.com>
2024-03-18 20:45:11 +0900

    feat: Question 및 Answer 모델 추가
    - SQLAlchemy를 사용하여 데이터베이스에 Question 및 Answer 모델 추가.
    - 각 모델은 `Base` 클래스에서 상속되며, `question` 테이블과 `answer` 테이블에 매핑됨.

---
+++ board/backend/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+
+    answers = relationship("Answer", back_populates="question")
+
+
+class Answer(Base):
+    __tablename__ = "answer"
+
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+    question_id = Column(Integer, ForeignKey("question.id"))
+
+    question = relationship("Question", back_populates="answers")
