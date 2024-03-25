5188e68a7b417f5cd910d8fdd0a1dc2c6bf27142
kmseunh <tmdgus8779@gmail.com>
2024-03-25 11:52:23 +0900

    feat: CORS 미들웨어를 사용하여 원격 호스트에서의 액세스 허용
    - FastAPI 애플리케이션에 CORS 미들웨어를 추가하여 원격 호스트에서의 액세스를 허용함
    - 허용되는 원격 호스트는 http://localhost:5173으로 제한됨
    - CORS 설정에서 credentials, methods 및 headers를 모두 허용하도록 구성함
    - question_router와 answer_router를 FastAPI 애플리케이션에 추가함

--- board/backend/main.py
+++ board/backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from api.answer import answer_router
+from api.question import question_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
 
-@app.get("/")
-def root():
-    return {"message": "Hello, World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(question_router.router)
+app.include_router(answer_router.router)
