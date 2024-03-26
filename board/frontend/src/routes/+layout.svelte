fa26863ccef9e83990380b5a7f934f2b48ef4154
kmseunh <tmdgus8779@gmail.com>
2024-03-26 12:45:04 +0900

    feat: 메인 네비게이션 추가
    - 사용자가 다른 페이지로 쉽게 이동할 수 있도록  네비게이션 추가.

--- board/frontend/src/routes/+layout.svelte
+++ board/frontend/src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
     import '../app.css';
+    import { goto } from '$app/navigation';
+
+    const navigateToPage = (url: string) => {
+        goto(url);
+    };
 </script>
+
+<div class="bg-gray-800 text-white py-4 px-4 flex justify-between items-center">
+    <button class="text-lg font-semibold" on:click={() => navigateToPage('/')}>
+        SHB
+    </button>
+    <div class="flex space-x-4">
+        <button
+            class="hover:text-gray-300"
+            on:click={() => navigateToPage('/user-login')}>로그인</button
+        >
+        <button
+            class="hover:text-gray-300"
+            on:click={() => navigateToPage('/user-create')}>회원가입</button
+        >
+    </div>
+</div>
 
 <slot />
