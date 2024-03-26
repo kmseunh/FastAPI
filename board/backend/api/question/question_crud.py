90712aa195dabd912c629dd0fa80ccd0d313af2b
kmseunh <tmdgus8779@gmail.com>
2024-03-26 13:15:16 +0900

    fix: 질문 목록 및 상세 조회 함수 업데이트
    - 질문 목록 및 상세 조회 함수를 업데이트했습니다.
    - get_question_list 함수에 skip 및 limit 매개변수를 추가하여 페이징을 지원하도록 했습니다.
    - get_question_list 함수에서는 총 질문 수와 페이징된 질문 목록을 반환하도록 수정되었습니다.

--- board/backend/api/question/question_crud.py
+++ board/backend/api/question/question_crud.py
@@ -7,9 +7,12 @@ import schema
 from models import Question
 
 
-def get_question_list(db: Session):
-    question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    return question_list
+def get_question_list(db: Session, skip: int = 0, limit: int = 10):
+    _question_list = db.query(Question).order_by(Question.create_date.desc())
+
+    total = _question_list.count()
+    question_list = _question_list.offset(skip).limit(limit).all()
+    return total, question_list
 
 
 def get_question(db: Session, question_id: int):
@@ -27,10 +30,20 @@ def create_question(db: Session, question_create: schema.QuestionCreate):
     db.commit()
 
 
-# def get_question_list(db: Session):
-#     query = text("SELECT * FROM question ORDER BY create_date DESC;")
-#     result = db.execute(query)
-#     return [dict(zip(result.keys(), row)) for row in result]
+# def get_question_list(db: Session, skip: int = 0, limit: int = 10):
+#     sql_query = """
+#     SELECT *
+#     FROM question
+#     ORDER BY create_date DESC
+#     OFFSET :skip LIMIT :limit;
+#     """
+
+#     total_query = "SELECT COUNT(*) FROM question;"
+
+#     question_list = db.execute(sql_query, {"skip": skip, "limit": limit}).fetchall()
+#     total = db.execute(total_query).scalar()
+
+#     return total, question_list
 
 
 # def get_question(db: Session, question_id: int):
