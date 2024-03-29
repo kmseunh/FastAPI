9eb7839bc321fb950b8ae92a0574820b660af8fa
kmseunh <tmdgus8779@gmail.com>
2024-03-30 00:10:50 +0900

    feat: 사용자 인증 및 세션 관련 서비스 추가
    - 사용자 인증(authenticate_user)을 위한 서비스 추가
    - 세션 생성(create_session) 및 세션을 통한 사용자 확인(get_user_by_session)을 위한 서비스 추가
    - 세션 만료 시간 설정 및 유효성 검사를 수행하는 기능 추가

---
+++ login_api/session_login/services/authentication.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from database import get_db
+from model import Sessions, User
+
+from .hashing import verify_password
+
+
+def create_session(user_id: int, db: Session = Depends(get_db)) -> Session:
+    expire_time = datetime.now() + timedelta(days=1)
+    new_session = Sessions(user_id=user_id, expire_time=expire_time)
+    db.add(new_session)
+    db.commit()
+    db.refresh(new_session)
+    return new_session
+
+
+def authenticate_user(
+    username: str, password: str, db: Session = Depends(get_db)
+) -> User:
+    user = db.query(User).filter(User.username == username).first()
+    if not user or not verify_password(password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+def get_user_by_session(session_id: str, db: Session = Depends(get_db)) -> User:
+    session = db.query(Sessions).filter(Sessions.session_id == session_id).first()
+    if not session or session.expire_time < datetime.now():
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Session expired or not found",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return session.user
