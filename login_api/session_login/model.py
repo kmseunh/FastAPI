5088ae81f96e8ce98949427e39545492b58d97ea
kmseunh <tmdgus8779@gmail.com>
2024-03-29 23:47:50 +0900

    feat: User, Session 모델 및 데이터베이스 테이블 추가 - SQLAlchemy의 Column 클래스를 사용하여 User 모델과 Session 모델을 추가하였습니다. - nullable 옵션을 사용하여 비어 있지 않아야 함을 명시하였습니다. - 각 클래스는 database 모듈에서 가져온 Base 클래스를 상속받아 데이터베이스 테이블을 정의합니다.

---
+++ login_api/session_login/model.py
@@ -0,0 +1,29 @@
+import uuid
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+
+    sessions = relationship("Sessions", back_populates="user")
+
+
+class Sessions(Base):
+    __tablename__ = "sessions"
+
+    session_id = Column(String, primary_key=True, default=str(uuid.uuid4))
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    create_time = Column(DateTime, default=func.now())
+    expire_time = Column(DateTime)
+
+    user = relationship("User", back_populates="sessions")
