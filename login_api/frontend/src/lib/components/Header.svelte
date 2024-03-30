435c7377185f85cf31276b05a48a1a08da7aadcb
kmseunh <tmdgus8779@gmail.com>
2024-03-31 03:27:49 +0900

    feat: Svelte 컴포넌트 추가 및 제목 동적 표시 기능 구현
    - Svelte 스크립트 블록에 제목을 동적으로 표시하는 변수인 title을 내보내는 기능을 추가함.

---
+++ login_api/frontend/src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let title: string;
+</script>
+
+<header class="mb-8">
+    <h2 class="text-2xl font-bold flex items-center justify-center">
+        {title}
+    </h2>
+</header>
