5a8faa94cea66c1ffea41c311b30e0a9e797ab0a
kmseunh <tmdgus8779@gmail.com>
2024-03-31 03:20:02 +0900

    feat: CORS 미들웨어 및 사용자 라우터 추가
    - 특정 origin에서의 요청을 허용하기 위해 CORS 미들웨어 추가
    - 추가적인 엔드포인트를 위해 사용자 라우터 포함

--- login_api/session_login/main.py
+++ login_api/session_login/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-from api import auth, some_resource
+from api import auth, some_resource, user_router
 
 app = FastAPI()
 
 
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(some_resource.router)
+app.include_router(user_router.router)
