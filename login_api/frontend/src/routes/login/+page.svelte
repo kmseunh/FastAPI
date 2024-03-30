40cf5859bafc3f154378a22aa92bb2343628f51d
kmseunh <tmdgus8779@gmail.com>
2024-03-31 03:32:40 +0900

    feat: 로그인 페이지 및 로그인 기능 추가
    - axios를 사용하여 로그인 양식 데이터를 서버로 전송하는 loginForm 함수를 구현함.
    - 로그인 양식에는 사용자 이름과 비밀번호 필드가 포함되어 있음.
    - 로그인 성공 시 받은 세션 ID를 스토어에 업데이트하고, 홈 페이지로 리다이렉트하는 기능을 추가함.

---
+++ login_api/frontend/src/routes/login/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import Header from '$lib/components/Header.svelte';
+    import { updateAuth } from '$lib/stores/auth';
+    import axios from 'axios';
+
+    let username: string = '';
+    let password: string = '';
+
+    const navigateTo = (url: string): void => {
+        goto(url);
+    };
+
+    const loginForm = async () => {
+        const formData = {
+            username,
+            password,
+        };
+
+        try {
+            const res = await axios.post('/api/login', formData);
+            console.log(formData);
+            if (res.status === 200) {
+                const sessionId = res.data.session_id;
+                updateAuth(sessionId);
+                alert('로그인 성공');
+                navigateTo('/');
+            }
+        } catch (error) {
+            console.error('Error occurred:', error);
+        }
+    };
+</script>
+
+<div class="min-h-screen flex flex-col items-center justify-center">
+    <Header title="Login Page" />
+
+    <section class="flex items-center justify-center">
+        <form class="flex flex-col space-y-4" on:submit|preventDefault>
+            <input
+                type="text"
+                placeholder="Username"
+                class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                bind:value={username}
+                required
+            />
+
+            <input
+                type="password"
+                placeholder="Password"
+                class="px-4 py-2 border border-gray-300 rounded-md focus:outline-gray-300 focus:border-blue-500"
+                bind:value={password}
+                required
+            />
+
+            <button
+                class="bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                on:click={() => loginForm()}>Login</button
+            >
+            <button
+                class="bg-gray-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                on:click={() => navigateTo('/join')}>Join</button
+            >
+        </form>
+    </section>
+</div>
