0f6201c8adbf78c6c5cb999c9e223e8343ced6f2
kmseunh <tmdgus8779@gmail.com>
2024-04-02 00:12:38 +0900

    feat: add SQLAlchemy User model

---
+++ user_auth/model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
