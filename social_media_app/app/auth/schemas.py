5781aed62354bd21613b97e420dfa52eba9204fc
kmseunh <tmdgus8779@gmail.com>
2024-04-06 19:09:23 +0900

    feat: 사용자(User) 스키마 정의
    - 'UserBase', 'UserCreate', 'UserUpdate', 'User' 클래스를 포함합니다.

---
+++ social_media_app/app/auth/schemas.py
@@ -0,0 +1,38 @@
+from datetime import date, datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from app.auth.enum import Gender
+
+
+class UserBase(BaseModel):
+    name: str
+    username: str
+    email: str
+    dob: Optional[date] = None
+    gender: Optional[Gender] = None
+    profile_pic: Optional[str] = None
+    bio: Optional[str] = None
+    location: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    dob: Optional[date] = None
+    gender: Optional[str] = None
+    profile_pic: Optional[str] = None
+    bio: Optional[str] = None
+    location: Optional[str] = None
+
+
+class User(UserBase):
+    id: int
+    create_dt: datetime
+
+    class Config:
+        orm_mod: True
