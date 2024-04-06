dd2701b80b1c45f131e52a4c10387f88169d51fd
kmseunh <tmdgus8779@gmail.com>
2024-04-06 19:10:06 +0900

    feat: 인증(Authentication) 관련 서비스 및 유틸리티 추가
    - 토큰 생성, 현재 사용자 가져오기, 사용자 생성, 사용자 인증, 사용자 업데이트 등의 기능이 구현되어 있습니다.

---
+++ social_media_app/app/auth/service.py
@@ -0,0 +1,119 @@
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+from starlette import status
+
+from app.auth.models import User
+from app.auth.schemas import User as UserSchema
+from app.auth.schemas import UserCreate, UserUpdate
+from app.core.config import settings
+from app.core.db import get_db
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="v1/auth/token")
+
+
+# username으로 조회
+async def get_user_by_username(username: str, db: Session = Depends(get_db)) -> User:
+    return db.query(User).filter(User.username == username).first()
+
+
+# 유저 체크
+async def existing_user(
+    username: str, email: str, db: Session = Depends(get_db)
+) -> bool:
+    db_username = db.query(User).filter(User.username == username).first()
+    print(db_username)
+    db_email = db.query(User).filter(User.email == email).first()
+
+    if db_username or db_email:
+        return True
+
+    return False
+
+
+# 토큰 생성
+async def create_access_token(user: UserSchema) -> str:
+    encode = {"sub": user.username, "id": user.id}
+    expires = datetime.utcnow() + timedelta(days=settings.EXPIRE_TIME)
+    encode.update({"exp": expires})
+    return jwt.encode(encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
+# 현재 user 가져오기
+async def get_current_user(
+    token: str = Depends(oauth2_bearer), db: Session = Depends(get_db)
+) -> User:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        username: str = payload.get("sub")
+        id: str = payload.get("id")
+        expires_timestamp = payload.get("exp")
+        print(expires_timestamp)
+        expires = datetime.utcfromtimestamp(expires_timestamp)
+        print(expires_timestamp)
+        if expires < datetime.utcnow():
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired"
+            )
+        if username is None or id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+            )
+        user = await get_user_by_username(username, db)
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
+            )
+        return user
+    except (JWTError, HTTPException) as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        ) from exc
+
+
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    try:
+        db_user = User(
+            name=user.name or None,
+            username=user.username.lower().strip(),
+            email=user.email.lower().strip(),
+            password_hash=bcrypt_context.hash(user.password),
+            dob=user.dob or None,
+            gender=user.gender or None,
+            bio=user.bio or None,
+            location=user.location or None,
+            profile_pic=user.profile_pic or None,
+        )
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except IntegrityError as exc:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username or email already exists",
+        ) from exc
+
+
+async def authenticate_user(username: str, password: str, db: Session) -> User:
+    user = await get_user_by_username(username, db)
+    if not user or not bcrypt_context.verify(password, user.password_hash):
+        return False
+    return user
+
+
+async def update_user(
+    db_user: UserSchema, user_update: UserUpdate, db: Session = Depends(get_db)
+):
+    for field, value in user_update.dict().items():
+        setattr(db_user, field, value or getattr(db_user, field))
+    db.commit()
