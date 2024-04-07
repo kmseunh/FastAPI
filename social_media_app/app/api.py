618c8a329dd0a8e20e348ca04c578d40c289f126
kmseunh <tmdgus8779@gmail.com>
2024-04-07 23:25:16 +0900

    feat: v1 API에 게시물용 추가 라우터 추가
    - 게시물 라우터에 대한 import 문 추가
    - v1 API 라우터에 게시물 라우터 포함

--- social_media_app/app/api.py
+++ social_media_app/app/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.auth.router import router as auth_router
+from app.post.router import router as post_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(auth_router)
+router.include_router(post_router)
