85e4b3c31eb37ae52e635aa4c6fc673642b6b17c
kmseunh <tmdgus8779@gmail.com>
2024-04-07 23:25:58 +0900

    feat: Alembic 마이그레이션에 다중 모델 지원 추가
    - PostBase 모델을 위한 import 문 추가
    - AuthBase와 함께 PostBase 메타데이터를 Alembic 마이그레이션에 포함

--- social_media_app/app/alembic/env.py
+++ social_media_app/app/alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.auth.models import Base as AuthBase
 from app.core.config import settings
+from app.post.models import Base as PostBase
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,6 +27,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = AuthBase.metadata
+target_metadata = PostBase.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
