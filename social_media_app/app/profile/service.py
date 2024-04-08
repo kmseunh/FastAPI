ae5afba540132ea9ccfe44c0dbfa7a328b7b2a83
kmseunh <tmdgus8779@gmail.com>
2024-04-09 02:14:45 +0900

    feat: 팔로우 관련 서비스 및 함수 추가
    - follow_svc: 사용자가 다른 사용자를 팔로우하는 기능을 추가했습니다. 이 함수는 팔로우 활동도 기록합니다.
    - unfollow_svc: 사용자가 다른 사용자의 팔로우를 취소하는 기능을 추가했습니다.
    - get_followers_svc: 특정 사용자의 팔로워 목록을 가져오는 기능을 추가했습니다.
    - get_followings_svc: 특정 사용자의 팔로잉 목록을 가져오는 기능을 추가했습니다.
    - check_follow_svc: 특정 사용자를 팔로우하고 있는지 확인하는 기능을 추가했습니다.

---
+++ social_media_app/app/profile/service.py
@@ -0,0 +1,142 @@
+from sqlalchemy.orm import Session
+
+from app.activity.models import Activity
+from app.auth.models import Follow, User
+from app.auth.service import existing_user, get_user_by_user_id, get_user_by_username
+from app.profile.schemas import FollowerList, FollowingList
+
+
+async def follow_svc(db: Session, follower: str, following: str):
+    db_follower_exist = await existing_user(follower, "", db)
+    db_following_exist = await existing_user(following, "", db)
+
+    if not db_follower_exist or not db_following_exist:
+        return False
+
+    db_follower = await get_user_by_username(follower, db)
+    db_following = await get_user_by_username(following, db)
+
+    db_follow = (
+        db.query(Follow)
+        .filter_by(follower_id=db_follower.id, following_id=db_following.id)
+        .first()
+    )
+
+    if db_follow:
+        return False
+
+    db_follow = Follow(follower_id=db_follower.id, following_id=db_following.id)
+    db.add(db_follow)
+
+    db_follower.followings_count += 1
+    db_following.followers_count += 1
+
+    follow_activity = Activity(
+        username=db_following.username,
+        followed_username=db_follower.username,
+        followed_user_pic=db_follower.profile_pic,
+    )
+
+    db.add(follow_activity)
+    db.commit()
+    db.refresh(follow_activity)
+
+
+async def unfollow_svc(db: Session, follower: str, following: str):
+    db_follower_exist = await existing_user(follower, "", db)
+    db_following_exist = await existing_user(following, "", db)
+
+    if not db_follower_exist or not db_following_exist:
+        return False
+
+    db_follower = await get_user_by_username(follower, db)
+    db_following = await get_user_by_username(following, db)
+
+    db_follow = (
+        db.query(Follow)
+        .filter_by(follower_id=db_follower.id, following_id=db_following.id)
+        .first()
+    )
+
+    if not db_follow:
+        return False
+
+    db.delete(db_follow)
+
+    db_follower.followings_count -= 1
+    db_following.followers_count -= 1
+
+    db.commit()
+
+
+async def get_followers_svc(db: Session, user_id: int) -> list[FollowerList]:
+    db_user = await get_user_by_user_id(db, user_id)
+
+    if not db_user:
+        return []
+
+    db_followers = (
+        db.query(Follow)
+        .filter_by(following_id=user_id)
+        .join(User, User.id == Follow.follower_id)
+        .all()
+    )
+
+    followers = []
+    for user in db_followers:
+        followers.append(
+            {
+                "profile_pic": user.follower.profie_pic,
+                "name": user.follower.name,
+                "username": user.follower.username,
+            }
+        )
+
+    return FollowerList(followers=followers)
+
+
+async def get_followings_svc(db: Session, user_id: int) -> list[FollowingList]:
+    db_user = await get_user_by_user_id(db, user_id)
+
+    if not db_user:
+        return []
+
+    db_followings = (
+        db.query(Follow)
+        .filter_by(follower_id=user_id)
+        .join(User, User.id == Follow.following_id)
+        .all()
+    )
+
+    following = []
+    for user in db_followings:
+        following.append(
+            {
+                "profile_pic": user.follower.profile_pic,
+                "name": user.follower.name,
+                "username": user.follower.username,
+            }
+        )
+
+    return FollowingList(followings=following)
+
+
+async def check_follow_svc(db: Session, current_user: str, user: str):
+    db_follower_exist = existing_user(current_user, "", db)
+    db_following_exist = existing_user(user, "", db)
+
+    if not db_follower_exist or not db_following_exist:
+        return False
+
+    db_follower = get_user_by_username(current_user, db)
+    db_following = get_user_by_username(user, db)
+
+    db_following = (
+        db.query(Follow)
+        .filter_by(follower_id=db_follower.id, following_id=db_following.id)
+        .first()
+    )
+
+    if db_following:
+        return True
+    return False
