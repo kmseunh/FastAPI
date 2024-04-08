272e224449436e570db94a59ed5689a0cfe4a584
kmseunh <tmdgus8779@gmail.com>
2024-04-09 02:08:32 +0900

    feat: User 모델에 팔로우 관련 필드 추가
    - follower_id와 following_id를 사용하여 유저 간의 팔로우 관계를 식별합니다.
    - followers 및 followings 관계를 사용하여 유저의 팔로워와 팔로잉 목록을 가져올 수 있습니다.
    - followers_count 및 followings_count를 추가하여 유저의 팔로워와 팔로잉 수를 추적합니다.

--- social_media_app/app/auth/models.py
+++ social_media_app/app/auth/models.py
@@ -1,11 +1,26 @@
 from datetime import datetime
 
-from sqlalchemy import DATE, Column, DateTime, Enum, Integer, String
+from sqlalchemy import DATE, Column, DateTime, Enum, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.auth.enum import Gender
 from app.core.db import Base
 from app.post.models import post_likes
+
+
+class Follow(Base):
+    __tablename__ = "follows"
+
+    follower_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    following_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+
+    follower = relationship(
+        "User", foreign_keys=[follower_id], back_populates="followers"
+    )
+
+    following = relationship(
+        "User", foreign_keys=[following_id], back_populates="followings"
+    )
 
 
 class User(Base):
@@ -30,3 +45,13 @@ class User(Base):
     liked_posts = relationship(
         "Post", secondary=post_likes, back_populates="like_by_users"
     )
+
+    followers = relationship(
+        Follow, foreign_keys=[Follow.following_id], back_populates="following"
+    )
+    followings = relationship(
+        Follow, foreign_keys=[Follow.follower_id], back_populates="follower"
+    )
+
+    followers_count = Column(Integer, default=0)
+    followings_count = Column(Integer, default=0)
