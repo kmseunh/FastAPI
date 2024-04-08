19a31171c16b0d36e6ece624d9dff47b1785ff48
kmseunh <tmdgus8779@gmail.com>
2024-04-08 16:16:35 +0900

    fix: post 라우터 및 서비스 업데이트
    - 포스트 관련 라우터 및 서비스 코드를 수정하였습니다.
    - `like_post_svc`, `unlike_post_svc` 함수명을 각각 `like_post`, `unlike_post`로 변경하였습니다.
    - `likes_post_svc`, `unlikes_post_svc` 함수명을 각각 `like_post_svc`, `unlike_post_svc`로 수정하였습니다.

--- social_media_app/app/post/router.py
+++ social_media_app/app/post/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
+from app.auth.schemas import User
 from app.auth.service import existing_user, get_current_user
 from app.core.db import get_db
 from app.post.schemas import Post, PostCreate
 from app.post.service import (
-    create_hashtags_svc,
     create_post_svc,
     delete_post_svc,
     get_post_from_post_id_svc,
@@ -13,9 +13,9 @@ from app.post.service import (
     get_posts_from_hashtag_svc,
     get_random_posts_svc,
     get_users_posts_svc,
+    like_post_svc,
     liked_users_post_svc,
-    likes_post_svc,
-    unlikes_post_svc,
+    unlike_post_svc,
 )
 
 router = APIRouter(prefix="/posts", tags=["posts"])
@@ -55,3 +55,64 @@ async def get_user_posts(username: str, db: Session = Depends(get_db)):
         return user_posts
     else:
         return []
+
+
+@router.get("/hashtag/{hashtag}")
+async def get_posts_from_hashtag(hashtag: str, db: Session = Depends(get_db)):
+    posts = await get_posts_from_hashtag_svc(hashtag, db)
+    return posts
+
+
+@router.get("/feed")
+async def get_random_posts(
+    db: Session = Depends(get_db), page: int = 1, limit: int = 10, hashtag: str = None
+):
+    return await get_random_posts_svc(db, page, limit, hashtag)
+
+
+@router.delete("/")
+async def delete_post(token: str, post_id: int, db: Session = Depends(get_db)):
+    user = await get_current_user(token, db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="you are not authorized"
+        )
+
+    post = await get_post_from_post_id_svc(post_id, db)
+    if post.author_id != user.id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="you are not authorized"
+        )
+
+    await delete_post_svc(post_id, db)
+
+
+@router.get("/like", status_code=status.HTTP_204_NO_CONTENT)
+async def like_post(post_id: int, username: str, db: Session = Depends(get_db)):
+    res, detail = await like_post_svc(post_id, username, db)
+    if res == False:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+@router.get("/unlike", status_code=status.HTTP_204_NO_CONTENT)
+async def unlike_post(post_id: int, username: str, db: Session = Depends(get_db)):
+    res, detail = await unlike_post_svc(post_id, username, db)
+    if res == False:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+@router.get("likes/{post_id}", response_model=list[User])
+async def users_like_post(post_id: int, db: Session = Depends(get_db)):
+    return await liked_users_post_svc(post_id, db)
+
+
+@router.get("/{post_id}", response_model=Post)
+async def get_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = await get_post_from_post_id_svc(post_id, db)
+    if not db_post:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="invaild post id"
+        )
+
+    return db_post
