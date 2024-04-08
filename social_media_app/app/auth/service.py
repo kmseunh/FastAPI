ade2f3df509d1e08e227f72f1d061cd12e1fffc7
kmseunh <tmdgus8779@gmail.com>
2024-04-09 02:09:38 +0900

    feat: 사용자 관련 기능 업데이트
    사용자 관련 기능을 업데이트하고 보완했습니다.
    - 사용자 조회를 위한 get_user_by_user_id 함수 추가
    - 현재 사용자 가져오기를 위한 get_current_user 함수 업데이트
    - 사용자 업데이트를 위한 update_user 함수 업데이트
    - 사용자가 이미 존재하는지 확인하기 위한 existing_user 함수에 이메일을 확인하는 기능 추가

--- social_media_app/app/auth/service.py
+++ social_media_app/app/auth/service.py
@@ -23,6 +23,10 @@ async def get_user_by_username(username: str, db: Session = Depends(get_db)) -> 
     return db.query(User).filter(User.username == username).first()
 
 
+async def get_user_by_user_id(db: Session, user_id: int) -> User:
+    return db.query(User).filter(User.id == user_id).first()
+
+
 # 유저 체크
 async def existing_user(
     username: str, email: str, db: Session = Depends(get_db)
@@ -48,24 +52,22 @@ async def create_access_token(user: UserSchema) -> str:
 async def get_current_user(
     token: str = Depends(oauth2_bearer), db: Session = Depends(get_db)
 ) -> User:
-    print(token)
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
         username: str = payload.get("sub")
-        id: str = payload.get("id")
+        user_id: str = payload.get("id")
         expires_timestamp = payload.get("exp")
         expires = datetime.utcfromtimestamp(expires_timestamp)
         if expires < datetime.utcnow():
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired"
             )
-        if username is None or id is None:
+        if username is None or user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Could not validate credentials",
             )
         user = await get_user_by_username(username, db)
-        print(user)
         if user is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
